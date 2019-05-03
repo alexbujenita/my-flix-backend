e75194af6e3bdef1d31ea5e96877cbff0b5f362f
Alex Bujenita <dekc89@gmail.com>
2019-05-03 10:38:43 +0100

    POST to have params

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,7 +1,12 @@
 module MoviesHelper
   
   def get_movies
-    movies = RestClient.get('http://api.open-notify.org/astros.json')
+    puts params
+    movies = RestClient.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=#{params[:page]}&api_key=#{Rails.application.credentials.dig(:themoviedb_api)}")
   end
+  
+  # For decrypting
+  # Rails.application.credentials.dig(:themoviedb_api)
+  # &page=#{params[:page]}
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  post '/movies', to: "movies#index"
   get '/movies', to: "movies#index"
 end
