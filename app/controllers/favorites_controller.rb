c531a118aaf88b2172cf520d93546d27ff1899c0
Alex Bujenita <dekc89@gmail.com>
2019-05-08 17:48:07 +0100

    minor API change

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
   def my_favorites
     if logged_in
-      render json: User.find(no_call_current_user[:id]).user_favorites
+      render json: User.find(no_call_current_user[:user_id]).user_favorites
     else 
       render json: {error: "Movies not available."}
     end
